681084af2ae1f24e77472e4815e7569a58b196e0
GuGaCacau <guga_cacau@hotmail.com>
2022-05-10 23:40:13 -0300

    GET Fetch Test

--- Site/scripts/prateleiras.js
+++ Site/scripts/prateleiras.js
@@ -1,21 +1,55 @@
-mostraProdutos = () => {
-  fetch("http://localhost:3000/ongs", {
+var estoques = [];
+
+async function mostraEstoques() {
+  const response = await fetch("http://localhost:3000/estoques", {
     method: "GET",
-    headers: {
+  });
+  const data = await response.json();
+  console.log(data.estoques);
+  estoques = data.estoques;
+  console.log(estoques[0]);
+
+  var tag = document.createElement("p");
+  var text = document.createTextNode(estoques[0].ID_ESTOQUE);
+  tag.appendChild(text);
+  var modal_container = document.getElementById("estoqueID");
+  modal_container.appendChild(tag);
+
+  var tag = document.createElement("p");
+  var text = document.createTextNode(estoques[0].QntTotal);
+  tag.appendChild(text);
+  var modal_container = document.getElementById("estoqueQntTotal");
+  modal_container.appendChild(tag);
+
+  var tag = document.createElement("p");
+  var text = document.createTextNode(estoques[0].QntMin);
+  tag.appendChild(text);
+  var modal_container = document.getElementById("estoqueQntMin");
+  modal_container.appendChild(tag);
+
+  var tag = document.createElement("p");
+  var text = document.createTextNode(estoques[0].fk_Produtos_ID_PRODUTO);
+  tag.appendChild(text);
+  var modal_container = document.getElementById("productID");
+  modal_container.appendChild(tag);
+}
+
+atualizaEstoque = () => {
+  fetch("http://localhost:3000/estoques", {
+    method: "PATCH",
+    header: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({}),
+    body: JSON.stringify({
+      qntTotal: "",
+      codbarras: "",
+    }),
   })
     .then((res) => {
       if (!res.ok) {
-        console.log("Fetch sem sucesso");
+        console.log("Fetch sem sucesso.");
       }
       res.json();
     })
-    .then((data) => console.log(data))
-    .catch((err) => console.error("ERRO: ", err));
-
-  data.body.map((val) => {
-    console.log(val.body);
-  });
+    .then((data) => console.log(data));
 };
